35fc0ac142942d95b18ad2794fc1b54f5d34d55e
Ahmad Zabadany <amdzy@proton.me>
2024-10-07 13:36:25 +0400

    feat: add create wallet command

---
+++ cmd/createwallet.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"amdzy/gochain/pkg/wallet"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var createWalletCmd = &cobra.Command{
+	Use:   "createwallet",
+	Short: "create wallet",
+	Long:  "create wallet",
+	Run: func(cmd *cobra.Command, args []string) {
+		wallets, err := wallet.NewWallets()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		address, err := wallets.CreateWallet()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = wallets.SaveToFile()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Your new address: %s\n", address)
+	},
+}
--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/vmihailenco/msgpack/v5 v5.4.1
 	go.etcd.io/bbolt v1.3.11
+	golang.org/x/crypto v0.27.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
