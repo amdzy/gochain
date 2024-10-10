bc98d7f7286d586e12622b2c6910cad645735c8d
Ahmad Zabadany <amdzy@proton.me>
2024-10-10 12:59:32 +0400

    feat: start node command

---
+++ cmd/startnode.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"amdzy/gochain/pkg/server"
+	"amdzy/gochain/pkg/wallet"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var minerAddress string
+var startNodeCmd = &cobra.Command{
+	Use:   "startnode",
+	Short: "Start Node",
+	Long:  "Start Node",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(minerAddress) > 0 {
+			validAddr := wallet.ValidateAddress(minerAddress)
+			if !validAddr {
+				log.Fatal("Wrong miner address!")
+			} else {
+				fmt.Println("Mining is on. Address to receive rewards: ", minerAddress)
+			}
+		}
+
+		err := server.StartServer("6000", minerAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
